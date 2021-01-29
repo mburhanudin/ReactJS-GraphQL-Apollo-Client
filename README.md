598a1dc8c070ddfc265132b124566bfda361fb61
Muhammad Burhanudin <muh.burhanudinn@gmail.com>
2021-01-29 19:25:12 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Run Project
+
+```npm start```
---
+++ src/index.js
@@ -0,0 +1,246 @@
+
+//QUERY
+
+// import React, { useState } from "react";
+// import { render } from "react-dom";
+// import {
+//   ApolloClient,
+//   InMemoryCache,
+//   ApolloProvider,
+//   useQuery,
+//   gql,
+//   NetworkStatus
+// } from "@apollo/client";
+
+// const client = new ApolloClient({
+//   uri: "https://71z1g.sse.codesandbox.io/",
+//   cache: new InMemoryCache()
+// });
+
+// const GET_DOGS = gql`
+//   {
+//     dogs {
+//       id
+//       breed
+//     }
+//   }
+// `;
+
+// function Dogs({ onDogSelected }) {
+//   const { loading, error, data } = useQuery(GET_DOGS);
+
+//   if (loading) return 'Loading...';
+//   if (error) return `Error! ${error.message}`;
+
+//   return (
+//     <select name="dog" onChange={onDogSelected}>
+//       {data.dogs.map(dog => (
+//         <option key={dog.id} value={dog.breed}>
+//           {dog.breed}
+//         </option>
+//       ))}
+//     </select>
+//   );
+// }
+
+
+
+// const GET_DOG_PHOTO = gql`
+//   query Dog($breed: String!) {
+//     dog(breed: $breed) {
+//       id
+//       displayImage
+//     }
+//   }
+// `;
+
+// function DogPhoto({ breed }) {
+//   const { loading, error, data, refetch, networkStatus } = useQuery(
+//     GET_DOG_PHOTO,
+//     {
+//       variables: { breed },
+//       notifyOnNetworkStatusChange: true,
+//     },
+//   );
+
+//   if (networkStatus === NetworkStatus.refetch) return 'Refetching!';
+//   if (loading) return null;
+//   if (error) return `Error! ${error}`;
+
+//   return (
+//     <div>
+//       <img src={data.dog.displayImage} style={{ height: 100, width: 100 }} />
+//       <button onClick={() => refetch()}>Refetch!</button>
+//     </div>
+//   );
+// }
+
+
+
+// function App() {
+//   const [selectedDog, setSelectedDog] = useState(null);
+
+//   function onDogSelected({ target }) {
+//     setSelectedDog(target.value);
+ 
+//   }
+
+//   return (
+//     <ApolloProvider client={client}>
+//       <div>
+//         <h2>Building Query components 🚀</h2>
+//         {selectedDog && <DogPhoto breed={selectedDog} />}
+//         <Dogs onDogSelected={onDogSelected} />
+     
+//       </div>
+//     </ApolloProvider>
+//   );
+// }
+
+
+// render(<App />, document.getElementById("root"));
+
+
+//MUTATIONS
+import React from "react";
+import { render } from "react-dom";
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  useQuery,
+  useMutation,
+  gql
+} from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "https://sxewr.sse.codesandbox.io/",
+  cache: new InMemoryCache()
+});
+
+const GET_TODOS = gql`
+  {
+    todos {
+      id
+      type
+    }
+  }
+`;
+
+const UPDATE_TODO = gql`
+  mutation UpdateTodo($id: String!, $type: String!) {
+    updateTodo(id: $id, type: $type) {
+      id
+      type
+    }
+  }
+`;
+
+function Todos() {
+  const { loading, error, data } = useQuery(GET_TODOS);
+  const [
+    updateTodo,
+    { loading: mutationLoading, error: mutationError }
+  ] = useMutation(UPDATE_TODO);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error :(</p>;
+
+  return data.todos.map(({ id, type }) => {
+    let input;
+
+    return (
+      <div key={id}>
+        <p>{type}</p>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            updateTodo({ variables: { id, type: input.value } });
+
+            input.value = "";
+          }}
+        >
+          <input
+            ref={node => {
+              input = node;
+            }}
+          />
+          <button type="submit">Update Todo</button>
+        </form>
+        {mutationLoading && <p>Loading...</p>}
+        {mutationError && <p>Error :( Please try again</p>}
+      </div>
+    );
+  });
+}
+
+const ADD_TODO = gql`
+  mutation AddTodo($type: String!) {
+    addTodo(type: $type) {
+      id
+      type
+    }
+  }
+`;
+
+function AddTodo() {
+  let input;
+  const [addTodo] = useMutation(ADD_TODO, {
+    update(
+      cache,
+      {
+        data: { addTodo }
+      }
+    ) {
+      cache.modify({
+        fields: {
+          todos(existingTodos = []) {
+            const newTodoRef = cache.writeFragment({
+              data: addTodo,
+              fragment: gql`
+                fragment NewTodo on Todo {
+                  id
+                  type
+                }
+              `
+            });
+            return existingTodos.concat(newTodoRef);
+          }
+        }
+      });
+    }
+  });
+
+  return (
+    <div>
+      <form
+        onSubmit={e => {
+          e.preventDefault();
+          addTodo({ variables: { type: input.value } });
+          input.value = "";
+        }}
+      >
+        <input
+          ref={node => {
+            input = node;
+          }}
+        />
+        <button type="submit">Add Todo</button>
+      </form>
+    </div>
+  );
+}
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <h2>Building Mutation components 🚀</h2>
+        <AddTodo />
+        <Todos />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+render(<App />, document.getElementById("root"));
